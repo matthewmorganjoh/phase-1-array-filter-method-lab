f30f4905076badfc9475fd54dc85445c7f0e85c2
Matthew Morgan <matthewmorganjoh@gmail.com>
2023-03-15 15:27:07 -0700

    complete array filter lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers, querey){
+    return drivers.filter(driver => driver.toLowerCase() === querey.toLowerCase());
+}
+
+function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.startsWith(query));
+  }
+
+  function matchName(drivers, query) {
+    return drivers.filter(driver => driver.name === query);
+  }
+   
